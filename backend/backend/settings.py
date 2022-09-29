4df5fafc32eb7e0d397db0fb94c2a4ec2699e80d
Jayakrishnan Jayu <jayu2442@gmail.com>
2022-09-29 14:37:01 +0530

    generate django static folder for production use

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -134,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 GRAPHENE = {
     "SCHEMA": "backend.schema.schema",
     'MIDDLEWARE': [
