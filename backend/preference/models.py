c831a81f6e5a76a7510293720d31f0514fb7c5b3
Jayakrishnan Jayu <jayu2442@gmail.com>
2022-11-08 20:18:59 +0530

    Add Prefernce models

--- backend/preference/models.py
+++ backend/preference/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Preference(models.Model):
+    preference_sem_identifier = models.ForeignKey(
+        'Identifier',
+        on_delete=models.PROTECT,
+    )
+    faculty = models.ForeignKey(
+        'user.Faculty',
+        on_delete=models.PROTECT,
+    )
+    course = models.ForeignKey(
+        'course.Course',
+        on_delete=models.PROTECT,
+    )
+    weigtage = models.PositiveSmallIntegerField()
+    experience = models.PositiveSmallIntegerField()
+    
+
+class Identifier(models.Model):
+    year = models.PositiveIntegerField()
+    is_even_sem = models.BooleanField()
+
+
+class Config(models.Model):
+    preference_count = models.PositiveSmallIntegerField(default = 3)
+    current_preference_sem = models.ForeignKey(
+        'Identifier',
+        on_delete=models.PROTECT
+    )
