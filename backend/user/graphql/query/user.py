08f958c1b1ead5d34d600c8911e5aa792367d0e8
Jayakrishnan Jayu <jayu2442@gmail.com>
2022-11-16 23:11:47 +0530

    feat: faculty query & faculty graphene type

--- backend/user/graphql/query/user.py
+++ backend/user/graphql/query/user.py
@@ -3,11 +3,15 @@ import graphene
 
 from django.contrib.auth import get_user_model
 
-from user.graphql.types.user import UserType
+from user.graphql.types.user import UserType, FacultyType
 from graphql_jwt.decorators import login_required
+from backend.api import APIException
+from user.models import Faculty
+
 class UserQueries(graphene.ObjectType):
     me = graphene.Field(UserType)
     users = graphene.List(UserType)
+    faculty = graphene.Field(FacultyType)
     
     
     @login_required
@@ -19,6 +23,17 @@ class UserQueries(graphene.ObjectType):
         if user.is_anonymous:
             raise Exception('Authentication Failure!')
         return user
+    
+    @login_required
+    def resolve_faculty(self, info):
+        user = info.context.user
+        try:
+            faculty = Faculty.objects.get(user_id = user.id)
+        except Faculty.DoesNotExist:
+            raise APIException("Faculty object not found")
+        return faculty
+        
+
 
 __all__ = [
     'UserQueries'
--- backend/user/graphql/types/user.py
+++ backend/user/graphql/types/user.py
@@ -1,10 +1,20 @@
 import graphene
 
 class UserType(graphene.ObjectType):
+    id = graphene.ID()
     email = graphene.String()
+    first_name = graphene.String()
+    last_name = graphene.String()
+    username = graphene.String()
 
-    
+
+class FacultyType(graphene.ObjectType):
+    user = graphene.Field(UserType)
+    track = graphene.String()
+    designation = graphene.String()
+
 
 __all__ = [
-    'UserType'
+    'UserType',
+    'FacultyType'
 ]
