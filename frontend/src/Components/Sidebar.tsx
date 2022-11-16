4a81aef453a62c3985f14d543d78a5aba65bd967
Pranavvks <pranav.vks@gmail.com>
2022-11-16 23:29:43 +0530

    feat:Course view buttons

--- frontend/src/Components/Sidebar.tsx
+++ frontend/src/Components/Sidebar.tsx
@@ -3,15 +3,15 @@ import AmritaLogo from "../assets/AmritaLogo.jpeg" ;
 import CoursesLogo from "../assets/book.svg" ;
 import MessagesLogo from "../assets/mail.svg"
 import LogoutLogo from "../assets/log-out.svg"
-import DashboardMain from "./DashboardMain";
+import MenuLogo from "../assets/menu.svg" ;
 
 const Sidebar = () => {
 
     const sidebar = (
-
-        <div className="max-w-none w-auto  min-h-screen grid grid-cols-[14rem,1fr] bg-slate-400"> 
+// 
+        <div className="max-w-none w-auto   min-h-screen grid grid-cols-[14rem,1fr] bg-gradient-to-r from-slate-400 to bg-slate-600"> 
         {/* Container */}
-        <div className="bg-gray-300 text-white p-10 rounded-2xl">
+        <div className="bg-gray-300 text-white p-10 rounded-tr-3xl rounded-tl-md rounded-b-xl">
             {/*Sidebar  */}
         <img src={AmritaLogo}></img>
         <div className="text-black text-sm">Course Management System</div>
@@ -38,7 +38,19 @@ const Sidebar = () => {
               </button>
            </div>
          
+           <div className="flex items-center">
+           
+           <img src={MenuLogo} className= "" />
+           
+               <button className="mb-3">
+              <div className="inline-block mt-3 ml-2 text-black " >Add Courses</div>
+              </button>
            </div>
+         
+
+           </div>
+         
+
          
            <div className="bottom-0 absolute -mr-60">
             <div className="flex items-center">
@@ -50,11 +62,21 @@ const Sidebar = () => {
            </div>
            </div>
      
-           <div className="p-10">My Courses</div>
-        
-         
+     {/* Main Dashboard */}
+          
+     <div className="p-10 text-gray-100 font-bold text-3xl">
+        My Courses
+        <br />
+        <br />
+    <div className="grid-flow-col text-lg ">
+        <button className="p-2" >All</button>
+        <button className="p-2">Active</button>
+        <button className="p-2">Upcoming</button>
+        <button className="p-2">Completed</button>
+    </div>
+        </div>
+ 
            
-             
                                
                         
 </div>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -7,7 +7,13 @@ module.exports = {
     fontSize: {
       sm: '0.6rem',
       base: '1rem',
-  
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
     }
   },
   plugins: [],
