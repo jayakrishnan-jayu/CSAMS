2af1142cecfecc53c93a93c13ed5e6ba84ae2c3d
Jayakrishnan Jayu <jayu2442@gmail.com>
2022-11-27 12:29:03 +0530

    modify curriculum json data structure

---
+++ backend/course/management/commands/convert.py
@@ -0,0 +1,33 @@
+import json
+
+files = ['BCA2018.txt', 'MCA2018.txt']
+for file in files:
+    output = {
+        'course': file[:-8],
+        'year': file[-8:-4],
+        'extra': {},
+        'semsters': {},
+    }
+    temp = {}
+    with open(file, 'r') as df:
+        lines = [l.strip() for l in df.readlines()]
+        start = 0
+        end = 0
+        for i in range(len(lines)):
+            line = lines[i]
+            if line == '':
+                end = i
+                temp[lines[start]] = lines[start+1:end]
+                start = i+1
+    for key in temp:
+        if key.startswith('+'):
+            output['extra'][key[1:-6]] = [{'code': line[:8], 'name': line[8:-6] ,'L': line[-5], 'T': line[-4], 'P': line[-3], 'C': line[-1]} for line in temp[key]]
+        else:
+            output['semsters'][int(key[-2:])] = {
+                'courses': [{'code': line[:7], 'name': line[8:-6] ,'L': line[-5], 'T': line[-4], 'P': line[-3], 'C': line[-1]} for line in temp[key] if not line.startswith('+')],
+                'extra': [line[1:-6]for line in temp[key] if line.startswith('+')],
+            }
+
+    with open(f'{output["course"]}{output["year"]}.json', 'w') as jf:
+        json.dump(output, jf, indent=4)
+            
