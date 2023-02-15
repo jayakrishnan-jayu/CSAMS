1d053ae32430e28350303016719e62b22d7ede89
Milind <milinddinesh@gmail.com>
2023-02-15 16:57:34 +0530

    Update Faculty API

--- backend/user/graphql/query/user.py
+++ backend/user/graphql/query/user.py
@@ -12,6 +12,7 @@ class UserQueries(graphene.ObjectType):
     me = graphene.Field(UserType)
     users = graphene.List(UserType)
     faculty = graphene.Field(FacultyType,faculty_id=graphene.ID(description= "Id of the faculty"))
+    faculties = graphene.Field(FacultyType)
     
     
     @login_required
@@ -27,13 +28,17 @@ class UserQueries(graphene.ObjectType):
         return user
     
     @login_required
-    def resolve_faculty(self, info, id:int=None):
+    def resolve_faculty(self, info, faculty_id:int=None):
         if id is not None:
             try:
-                faculty = Faculty.objects.get(user_id = id)
+                faculty = Faculty.objects.get(user_id = faculty_id)
             except Faculty.DoesNotExist:
                 raise APIException("Faculty object not found")
         return faculty
+
+    @login_required
+    def resolve_faculties(self,info):
+        faculties = Faculty.objects
 
 
     # @login_required
