ba2cc2d3f77aa9e1411190cc199c97cb7fa35a44
Milind <milinddinesh@gmail.com>
2023-02-26 17:42:00 +0530

    completed allocationData api

--- backend/allocation/graphql/types/allocation.py
+++ backend/allocation/graphql/types/allocation.py
@@ -2,9 +2,10 @@ import graphene
 # from allocation.models import CourseAllocation, LabAllocation
 from course.graphql.types.course import CourseType
 from user.graphql.types.user import FacultyType
+from user.graphql.types.user import UserType
 from backend.api import APIException
 from typing import List
 
 class CourseAndFacultyType(graphene.ObjectType):
     courses = graphene.Field(CourseType)
-    faculties = graphene.Field(FacultyType)
+    faculties = graphene.Field(UserType)
