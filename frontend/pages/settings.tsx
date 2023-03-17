83951d4bf9a8171c710ad5cd433ae8ab9dee95de
Anandu P <anandup0122@gmail.com>
2023-03-17 11:37:44 +0530

    use query for updating details and add toast

--- frontend/pages/settings.tsx
+++ frontend/pages/settings.tsx
@@ -1,20 +1,51 @@
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { FacultyContext } from "@/components/layout/context/facultycontext";
+import { Toast } from "primereact/toast";
 
 const Settings = () => {
-
   const { facultyData } = useContext(FacultyContext);
   console.log("FacultData, ", facultyData);
-  const [firstName, setFirstName] = useState("Anandu");
-  const [lastName, setLastName] = useState("P");
-  const [designation, setDesignation] = useState("Teaching");
-  const [track, setTrack] = useState("Something Something");
-  const [email, setEmail] = useState("anandup0122@gmail.com");
+  const [firstName, setFirstName] = useState(
+    facultyData?.faculty?.user?.firstName
+  );
+  const [lastName, setLastName] = useState(
+    facultyData?.faculty?.user?.lastName
+  );
+  const designation = facultyData?.faculty?.designation || "undefined";
+
+  const track = facultyData?.faculty?.track || "undefined";
+  const email = facultyData?.faculty?.user?.email || "undefined";
+
+  const toast = useRef(null);
+
+  const onProfileUpdate = () => {
+    if (
+      firstName?.length == 0 ||
+      firstName?.length == null ||
+      lastName?.length == 0 ||
+      lastName?.length == null
+    ) {
+      toast.current.show({
+        severity: "error",
+        summary: "Error",
+        detail: "Message Content",
+        life: 3000,
+      });
+    } else {
+      toast.current.show({
+        severity: "success",
+        summary: "Success",
+        detail: "Message Content",
+        life: 3000,
+      });
+    }
+  };
   return (
     <div className="card">
       <h5>Account Settings</h5>
+      <Toast ref={toast} />
       <div className="grid p-fluid mt-5">
         <div className="field col-12 md:col-6">
           <span className="">
@@ -79,7 +110,11 @@ const Settings = () => {
         </div>
       </div>
       <div className="flex flex-wrap">
-        <Button label="Save" className="p-button-info" />
+        <Button
+          label="Save"
+          onClick={onProfileUpdate}
+          className="p-button-info"
+        />
       </div>
     </div>
   );
