06f2b64b3daf49d7d368ee6c18342601b028af03
Milind <milinddinesh@gmail.com>
2023-03-18 13:20:20 +0530

    edit in resolve_allocation

--- backend/allocation/graphql/query/courseAllocation.py
+++ backend/allocation/graphql/query/courseAllocation.py
@@ -15,12 +15,13 @@ from django.db.models import Sum
 class AllocationQueries(graphene.ObjectType):
     allocation = graphene.List(
         CourseAndFacultyType,
+        # faculty_id = graphene.ID(description="ID of faculty"),
         filter = graphene.Argument(AllocationFilterInput,required=True))   
      
     @login_required
     def resolve_allocation(self,info,filter):
         batches = Batch.objects.annotate(odd=F('sem') % 2, sem_year=F('year')+(F('sem')-1)/2).filter(odd=not filter.is_even_sem , sem_year=filter.year)
         courses_in_batches = Course.objects.filter(batch__in=batches)
-        CourseAllocation.objects.filter(course__in=courses_in_batches)
-        allocations = CourseAllocation.objects.all()
+        allocations = CourseAllocation.objects.filter(course__in=courses_in_batches)
+        # allocations = CourseAllocation.objects.all()
         return allocations
