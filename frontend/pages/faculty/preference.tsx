ed25786a34c63b7d676b721a0cfdde6c8282bbae
Anandu P <anandup0122@gmail.com>
2023-05-03 09:10:20 +0530

    Update: added more tables and mutation call

--- frontend/pages/faculty/preference.tsx
+++ frontend/pages/faculty/preference.tsx
@@ -1,79 +1,95 @@
-import { useCoursesQuery } from "@/graphql/generated/graphql";
+import {
+  useCoursesQuery,
+  useAddPreferenceMutation,
+} from "@/graphql/generated/graphql";
 import { Dropdown } from "primereact/dropdown";
 import { useRef, useState } from "react";
 import { Button } from "primereact/button";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
 import { Dialog } from "primereact/dialog";
+import { Toast } from "primereact/toast";
+import { InputNumber } from "primereact/inputnumber";
 
 const FacultyPreference = () => {
   let emptyCourseData = {
-    code: "",
-    name: "",
+    courseId: "",
+    experience: 0,
+    weightage: 0,
   };
-  const [programValue, setProgramValue] = useState("");
-  const [yearValue, setYearValue] = useState("");
-  const [semValue, setSemValue] = useState("");
-
-  const [result] = useCoursesQuery({variables:{PROGRAM:"BCA", SEM:6, YEAR:2018}});
+  const [result] = useCoursesQuery();
   const { fetching, data, error } = result;
+  const [productDialog, setProductDialog] = useState(false);
+  const [product, setProduct] = useState(emptyCourseData);
+  const [dialogShown, setDialogShown] = useState(true);
+  const [newPreferences, updatePreferences] = useAddPreferenceMutation();
+
+  const toast = useRef(null);
+  const dt = useRef(null);
 
   if (error) return <div>{error.toString()}</div>;
   let courses: any[] | undefined = [];
-  if (data?.courses?.courses) {
-    courses = data?.courses?.courses;
+  if (data?.courses) {
+    courses = data?.courses;
   }
 
-  const dt = useRef(null);
+  if (newPreferences.error?.message && !dialogShown) {
+    toast.current.show({
+      severity: "error",
+      summary: "Error updating workload",
+      detail: newPreferences.error.message,
+      life: 3000,
+    });
+    setDialogShown(true);
+  }
 
-  const programDropdownValues = [
-    {
-      name: "BCA",
-      code: "bca",
-    },
-    {
-      name: "MCA",
-      code: "mca",
-    },
-  ];
+  if (newPreferences.data?.addPreference?.response && !dialogShown) {
+    toast.current.show({
+      severity: "success",
+      summary: "Workload Updated",
+      life: 3000,
+    });
+    setDialogShown(true);
+  }
 
-  const yearDropdownValues = [
-    {
-      name: "2018",
-      code: "2018",
-    },
-    {
-      name: "2020",
-      code: "2020",
-    },
-  ];
+  const hideDialog = () => {
+    setProductDialog(false);
+  };
 
-  const semDropdownValues = [
-    {
-      name: "S1",
-      code: "s1",
-    },
-    {
-      name: "S2",
-      code: "s2",
-    },
-    {
-      name: "S3",
-      code: "s3",
-    },
-    {
-      name: "S4",
-      code: "s4",
-    },
-    {
-      name: "S5",
-      code: "s5",
-    },
-    {
-      name: "S6",
-      code: "s6",
-    },
-  ];
+  const saveProduct = async () => {
+    setDialogShown(false);
+    await updatePreferences({
+      COURSEID: product.courseId,
+      EXPERIENCE: product.experience,
+      WEIGHTAGE: product.weightage,
+    });
+    setProduct(emptyCourseData);
+    setProductDialog(false);
+  };
+
+  const editCourses = (product) => {
+    setProduct({ ...product });
+    setProductDialog(true);
+  };
+
+  const onInputNumberChange = (e, name: string) => {
+    const val = e.value || 0; // TODO: parse to integer
+    let _product = { ...product };
+    if (name === "preference") _product.preference = val;
+    else if (name === "experience") _product.experience = val;
+    setProduct(_product);
+  };
+
+  const courseIDBodyTemplate = (rowData) => {
+    return (
+      <>
+        <span className="p-column-title">Course Id</span>
+        <span className={`generic-badge code-${rowData?.id.toLowerCase()}`}>
+          {rowData?.id}
+        </span>
+      </>
+    );
+  };
 
   const courseCodeBodyTemplate = (rowData) => {
     return (
@@ -97,67 +113,105 @@ const FacultyPreference = () => {
     );
   };
 
+  const courseCreditBodyTemplate = (rowData) => {
+    return (
+      <>
+        <span className="p-column-title">Credit</span>
+        <span className={`generic-badge code-${rowData?.credit}`}>
+          {rowData?.credit}
+        </span>
+      </>
+    );
+  };
+
+  // const courseExtraBodyTemplate = (rowData) => {
+  //   return (
+  //     <>
+  //       <span className="p-column-title">Is Extra</span>
+  //       <span className={`generic-badge code-${rowData?.isExtra}`}>
+  //         {rowData?.isExtra}
+  //       </span>
+  //     </>
+  //   );
+  // };
+
+  const courseProgramBodyTemplate = (rowData) => {
+    return (
+      <>
+        <span className="p-column-title">Program</span>
+        <span className={`generic-badge code-${rowData?.program}`}>
+          {rowData?.program}
+        </span>
+      </>
+    );
+  };
+
+  const courseYearBodyTemplate = (rowData) => {
+    return (
+      <>
+        <span className="p-column-title">Curriculum Year</span>
+        <span className={`generic-badge code-${rowData?.curriculumYear}`}>
+          {rowData?.curriculumYear}
+        </span>
+      </>
+    );
+  };
+
+  const courseBatchYearBodyTemplate = (rowData) => {
+    return (
+      <>
+        <span className="p-column-title">Batch Year</span>
+        <span className={`generic-badge code-${rowData?.batchYear}`}>
+          {rowData?.batchYear}
+        </span>
+      </>
+    );
+  };
+
+  const courseSemBodyTemplate = (rowData) => {
+    return (
+      <>
+        <span className="p-column-title">Sem</span>
+        <span className={`generic-badge code-${rowData?.sem}`}>
+          {rowData?.sem}
+        </span>
+      </>
+    );
+  };
+
   const actionBodyTemplate = (rowData) => {
     return (
       <>
         <Button
           icon="pi pi-chevron-up"
           className="p-button-rounded p-button-success mr-2"
-          // onClick={() => editWorkload(rowData)}
+          onClick={() => editCourses(rowData)}
         />
       </>
     );
   };
 
+  const productDialogFooter = (
+    <>
+      <Button
+        label="Cancel"
+        icon="pi pi-times"
+        className="p-button-text"
+        onClick={hideDialog}
+      />
+      <Button
+        label="Save"
+        icon="pi pi-check"
+        className="p-button-text"
+        onClick={saveProduct}
+      />
+    </>
+  );
+
   return (
     <div className="">
-      {/* <div className="card flex gap-5">
-        <div className="">
-          <h5>Program</h5>
-          <Dropdown
-            value={programValue}
-            onChange={(e) => {
-              setProgramValue(e.value);
-            }}
-            options={programDropdownValues}
-            optionLabel="name"
-            placeholder="Select"
-          />
-        </div>
-        <div className="">
-          <h5>Year</h5>
-          <Dropdown
-            value={yearValue}
-            onChange={(e) => {
-              setYearValue(e.value);
-            }}
-            options={yearDropdownValues}
-            optionLabel="name"
-            placeholder="Select"
-          />
-        </div>
-
-        <div className="">
-          <h5>Semester</h5>
-          <Dropdown
-            value={semValue}
-            onChange={(e) => {
-              setSemValue(e.value);
-            }}
-            options={semDropdownValues}
-            optionLabel="name"
-            placeholder="Select"
-          />
-        </div>
-        <div className="">
-          <Button
-            icon="pi pi-sort-down"
-            className="p-button-rounded p-button-success mt-5 mr-2"
-            // onClick={handleClick}
-          />
-        </div>
-      </div> */}
       <div className="card">
+        <Toast ref={toast} />
         <DataTable
           ref={dt}
           value={courses}
@@ -169,6 +223,13 @@ const FacultyPreference = () => {
           emptyMessage="No courses info found."
           responsiveLayout="scroll"
         >
+          {/* <Column
+            field="id"
+            header="Course Id"
+            sortable
+            body={courseIDBodyTemplate}
+            headerStyle={{ minWidth: "10rem" }}
+          /> */}
           <Column
             field="code"
             header="Course Code"
@@ -184,10 +245,91 @@ const FacultyPreference = () => {
             headerStyle={{ minWidth: "10rem" }}
           />
           <Column
+            field="credit"
+            header="Credit"
+            sortable
+            body={courseCreditBodyTemplate}
+            headerStyle={{ minWidth: "5rem" }}
+          />
+          {/* <Column
+            field="isExtra"
+            header="Is Extra"
+            sortable
+            body={courseExtraBodyTemplate}
+            headerStyle={{ minWidth: "5rem" }}
+          /> */}
+          <Column
+            field="program"
+            header="Program"
+            sortable
+            body={courseProgramBodyTemplate}
+            headerStyle={{ minWidth: "5rem" }}
+          />
+          <Column
+            field="curriculumYear"
+            header="Curriculum Year"
+            sortable
+            body={courseYearBodyTemplate}
+            headerStyle={{ minWidth: "5rem" }}
+          />
+          <Column
+            field="batchYear"
+            header="Batch Year"
+            sortable
+            body={courseBatchYearBodyTemplate}
+            headerStyle={{ minWidth: "5rem" }}
+          />
+          <Column
+            field="sem"
+            header="Sem"
+            sortable
+            body={courseSemBodyTemplate}
+            headerStyle={{ minWidth: "5rem" }}
+          />
+          <Column
             body={actionBodyTemplate}
             headerStyle={{ minWidth: "5rem" }}
           />
         </DataTable>
+        <Dialog
+          visible={productDialog}
+          style={{ width: "450px" }}
+          header="Preference"
+          modal
+          className="p-fluid"
+          footer={productDialogFooter}
+          onHide={hideDialog}
+        >
+          <div className="formgrid grid">
+            <div className="field col">
+              <label>Course ID</label>
+              <div className={`generic-badge track-${product?.id}`}>
+                {product?.id}
+              </div>
+            </div>
+          </div>
+
+          <div className="formgrid grid">
+            <div className="field col">
+              <label>Preference</label>
+              <InputNumber
+                id="preference"
+                value={product.preference}
+                onValueChange={(e) => onInputNumberChange(e, "preference")}
+                integeronly
+              />
+            </div>
+            <div className="field col">
+              <label>Experience</label>
+              <InputNumber
+                id="experience"
+                value={product.experience}
+                onValueChange={(e) => onInputNumberChange(e, "experience")}
+                integeronly
+              />
+            </div>
+          </div>
+        </Dialog>
       </div>
     </div>
   );
