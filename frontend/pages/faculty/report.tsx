6fa6cd555cd9047e49c5e060c9a95604131dcc61
Anandu P <anandup0122@gmail.com>
2023-05-03 10:12:39 +0530

    add: added a UI to report page

--- frontend/pages/faculty/report.tsx
+++ frontend/pages/faculty/report.tsx
@@ -1,13 +1,43 @@
 import { MetaDataContext } from "@/components/layout/context/metadatacontext";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0"
-import { useContext } from "react";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useContext, useState } from "react";
+import { Dropdown } from "primereact/dropdown";
+import { Button } from "primereact/button";
+import CourseBook from "@/components/report/courseBook";
+import BatchWise from "@/components/report/batchWise";
 
-export default function Report() { 
+export default function Report() {
   const { metaData } = useContext(MetaDataContext);
-  
+
+  const [dropdownValue, setDropdownValue] = useState(null);
+
+  const dropdownValues = [
+    {
+      name: "Course Book",
+      code: "courseBook",
+    },
+    {
+      name: "Batch wise",
+      code: "batchWise",
+    },
+  ];
+
   return (
-    <div>Report for  {metaData?.metadata?.user?.username}</div>
-  )
+    <div>
+      <div className="card">
+        <Dropdown
+          value={dropdownValue}
+          onChange={(e) => setDropdownValue(e.value)}
+          options={dropdownValues}
+          optionLabel="name"
+          placeholder="Select"
+        />
+        {dropdownValue?.code === "courseBook" && <CourseBook />}
+        {dropdownValue?.code === "batchWise" && <BatchWise />}
+      </div>
+      <div className=""></div>
+    </div>
+  );
 }
 
 export const getServerSideProps = withPageAuthRequired();
